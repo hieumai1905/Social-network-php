a51a9dd36be033269e8d309d3473c1bcde5af711
Astolfo-Saber <hungtd2180@gmail.com>
2023-10-29 03:06:47 +0700

    edit route connection for CommentDAO ~2

--- src/DAO/comment/CommentDAO.php
+++ src/DAO/comment/CommentDAO.php
@@ -4,7 +4,7 @@ namespace DAO\comment;
 use PDO;
 use models\Comment;
 
-require_once 'src/DAO/Databases/ConnectDatabase.php';
+require_once 'src/DAO/databases/ConnectDatabase.php';
 require_once 'ICommentDAO.php';
 require_once 'src/models/Comment.php';
 class CommentDAO implements ICommentDAO
