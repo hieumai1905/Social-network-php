ea800945ccbada0a15d4d46a2e9b4b81813e272b
Phong <hongphongle774@gmail.com>
2023-10-31 02:52:30 +0700

    add function editUser

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -4,6 +4,7 @@ namespace controllers;
 
 
 use Google\Exception;
+use Google\Service\AlertCenter\User;
 use https\Response;
 use https\Status;
 use Media;
@@ -34,11 +35,15 @@ class UserController
         $user = $this->userService->getById($id);
         return Response::view('views/Profile',['user'=>$user]);
     }
-
     public function getUser($id) {
         $user = $this->userService->getById($id);
         $data = Mapper::mapModelToJson($user);
         return Response::apiResponse(Status::OK,'success',$data);
+    }
+    public function getUserToEditProfile() {
+        $userLogin = unserialize($_SESSION['user-login']);
+        $user = $this->userService->getById($userLogin->getUserId());
+        return Response::view('views/Edit-Information',['user'=>$user]);
     }
     public function findUserByContent($content) {
         try {
@@ -55,6 +60,35 @@ class UserController
     //---------------------------------------------------------------------
     //---------------------------HTTP POST--------------------------------
 
+
+    //---------------------------------------------------------------------
+
+    //---------------------------HTTP PUT--------------------------------
+    public function updateUser() {
+        try {
+            $userLogin = unserialize($_SESSION['user-login']);
+            $user = new \models\User();
+            $json = Response::getJson();
+            $user->setUserId($userLogin->getUserId());
+            $user->setFullName($json['full_name']);
+            $user->setAvatar($userLogin->getAvatar());
+            $user->setPassword($userLogin->getPassword());
+            $user->setEmail($userLogin->getEmail());
+            $user->setDob($json['dob']);
+            $user->setAddress($json['address']);
+            $user->setGender($json['gender']);
+            $user->setPhone($userLogin->getPhone());
+            $user->setStatus($userLogin->getStatus());
+            $user->setUserRole($userLogin->getUserRole());
+            $user->setAboutMe($json['about_me']);
+            $user->setCoverImage($userLogin->getCoverImage());
+            $user->setRegisterAt($userLogin->getRegisterAt());
+            $this->userService->update($user);
+            return Response::apiResponse(Status::OK,'success',null);
+        }catch (Exception $e) {
+            return Response::apiResponse(Status::INTERNAL_SERVER_ERROR,$e->getMessage(),null);
+        }
+    }
 
     //---------------------------------------------------------------------
 }
