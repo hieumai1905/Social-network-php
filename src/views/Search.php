a5c6a664fa843a65dc718142ebfdca058b1fe292
Phong <hongphongle774@gmail.com>
2023-11-01 00:28:52 +0700

    Fix search user

--- src/views/Search.php
+++ src/views/Search.php
@@ -15,11 +15,13 @@
                         </div>
                         <div id="searchresult" class="row ps-2 pe-2">
                             <?php
+                                $i = 0;
                                 foreach ($data['users'] as $item) {
                                     $fullName = $item->getFullName();
                                     $email = $item->getEmail();
-                                    $urlProfile = 'http://localhost:8080/users/'.$item->getUserId();
-                                    echo "<div class='col-md-12 col-sm-4 pe-2 ps-2 phong1'>
+                                    $urlProfile = 'http://localhost:8080/users/' . $item->getUserId();
+                                    if ($data['relation'][$i] == null or $data['relation'][$i]->getTypeRelation() == 'FRIEND') {
+                                        echo "<div class='col-md-12 col-sm-4 pe-2 ps-2 phong1'>
                             <div class='card d-block border-0 shadow-xss rounded-3 overflow-hidden mb-3 phong2'>
                                 <div class='card-body w-100 ps-3 pe-3 pb-4 phong3'>
                                     <figure class='avatar mb-0 position-relative w65 z-index-1 phong4'>
@@ -32,6 +34,45 @@
                                 </div>
                             </div>
                         </div>";
+                                        $i += 1;
+                                    } else {
+                                        if ($data['relation'][$i]->getTypeRelation() == 'BLOCK') {
+                                            $i += 1;
+                                            continue;
+                                        }
+                                        else if ($data['relation'][$i]->getTypeRelation() == 'WAITING') {
+                                            echo "<div class='col-md-12 col-sm-4 pe-2 ps-2 phong1'>
+                            <div class='card d-block border-0 shadow-xss rounded-3 overflow-hidden mb-3 phong2'>
+                                <div class='card-body w-100 ps-3 pe-3 pb-4 phong3'>
+                                    <figure class='avatar mb-0 position-relative w65 z-index-1 phong4'>
+                                        <img style='width:65px; height:65px; border-radius:50px;' src='/public/images/user-7.png' alt='image' class='phong5'>
+                                    </figure>
+                                    <div class='clearfix phong6'></div>
+                                    <h4 class='fw-700 font-xsss phong7'>$fullName</h4>
+                                    <p class='fw-500 font-xsssss text-grey-500 phong8'>$email</p>
+                                    <a style='background-color:blue' href='$urlProfile' class='profile phong9 mt-0 btn pt-2 pb-2 ps-3 pe-3 lh-24 ls-3 d-inline-block rounded-xl font-xsssss fw-700 ls-lg text-white'>View Profile</a>
+                                </div>
+                            </div>
+                        </div>";
+                                            $i += 1;
+                                        }
+                                        else if ($data['relation'][$i]->getTypeRelation() == 'REQUEST') {
+                                            echo "<div class='col-md-12 col-sm-4 pe-2 ps-2 phong1'>
+                            <div class='card d-block border-0 shadow-xss rounded-3 overflow-hidden mb-3 phong2'>
+                                <div class='card-body w-100 ps-3 pe-3 pb-4 phong3'>
+                                    <figure class='avatar mb-0 position-relative w65 z-index-1 phong4'>
+                                        <img style='width:65px; height:65px; border-radius:50px;' src='/public/images/user-7.png' alt='image' class='phong5'>
+                                    </figure>
+                                    <div class='clearfix phong6'></div>
+                                    <h4 class='fw-700 font-xsss phong7'>$fullName</h4>
+                                    <p class='fw-500 font-xsssss text-grey-500 phong8'>$email</p>
+                                    <a style='background-color:yellow' href='$urlProfile' class='profile phong9 mt-0 btn pt-2 pb-2 ps-3 pe-3 lh-24 ls-3 d-inline-block rounded-xl font-xsssss fw-700 ls-lg text-white'>View Profile</a>
+                                </div>
+                            </div>
+                        </div>";
+                                            $i += 1;
+                                        }
+                                    }
                                 }
                             ?>
                         </div>
