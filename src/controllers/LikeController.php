29d7a1d32885fd3b44ed6a18df7a49d6b86b967a
Astolfo-Saber <hungtd2180@gmail.com>
2023-10-31 10:21:03 +0700

    create LikeController ~2

--- src/controllers/LikeController.php
+++ src/controllers/LikeController.php
@@ -17,10 +17,10 @@ class LikeController
         $this->likeService = $likeService;
     }
     //-------------------------------HTTP GET --------------------------------
-    //HTTP GET (/like/post/$post_id/$user_id)
-    public function getLikeOfPostByUser($post_id, $user_id){
+    //HTTP GET (/like/post/$postId/$userId)
+    public function getLikeOfPostByUser($postId, $userId){
         try{
-            $like = $this->likeService->getLikeOfPostByUserId($post_id, $user_id);
+            $like = $this->likeService->getLikeOfPostByUserId($postId, $userId);
             $data = Mapper::mapModelToJson($like);
             return Response::apiResponse(Status::OK, 'get like of post by user successfully', $data);
         } catch (Exception $e) {
@@ -47,10 +47,89 @@ class LikeController
             return Response::apiResponse(Status::INTERNAL_SERVER_ERROR, $e->getMessage(), null);
         }
     }
+    //HTTP GET (/like/count/post/$postId)
+    public function getCountLikePost($postId){
+        try{
+            $count = $this->likeService->getLikeCountOfPost($postId);
+            return Response::apiResponse(Status::OK, 'count like post successfully', $count);
+        }catch (Exception $e) {
+            return Response::apiResponse(Status::INTERNAL_SERVER_ERROR, $e->getMessage(), null);
+        }
+    }
+    //HTTP GET (/like/count/comment/$commentId)
+    public function getCountLikeComment($commentId){
+        try{
+            $count = $this->likeService->getLikeCountOfComment($commentId);
+            return Response::apiResponse(Status::OK, 'count like comment successfully', $count);
+        }catch (Exception $e) {
+            return Response::apiResponse(Status::INTERNAL_SERVER_ERROR, $e->getMessage(), null);
+        }
+    }
+    //HTTP GET (/like/count/comment/reply/$commentReplyId)
+    public function getCountLikeCommentReply($commentReplyId){
+        try{
+            $count = $this->likeService->getLikeCountOfCommentReply($commentReplyId);
+            return Response::apiResponse(Status::OK, 'count like comment reply successfully', $count);
+        }catch (Exception $e) {
+            return Response::apiResponse(Status::INTERNAL_SERVER_ERROR, $e->getMessage(), null);
+        }
+    }
     //-------------------------------HTTP POST --------------------------------
-    //HTTP POST (/comment/reply)
-    //-------------------------------HTTP POST --------------------------------
-    //HTTP POST (/comment/reply)
-    //-------------------------------HTTP POST --------------------------------
-    //HTTP POST (/comment/reply)
+    //HTTP POST (/like/post/$postId/$userId)
+    public function likePost($postId, $userId){
+        try{
+            $this->likeService->addLikePost($postId, $userId);
+            return Response::apiResponse(Status::OK, 'like post successfully', null);
+        }catch (Exception $e) {
+            return Response::apiResponse(Status::INTERNAL_SERVER_ERROR, $e->getMessage(), null);
+        }
+
+    }
+    //HTTP POST (/like/comment/$commentId/$userId)
+    function likeComment($commentId, $userId){
+        try{
+            $this->likeService->addLikeComment($commentId, $userId);
+            return Response::apiResponse(Status::OK, 'like comment successfully', null);
+        }catch (Exception $e) {
+            return Response::apiResponse(Status::INTERNAL_SERVER_ERROR, $e->getMessage(), null);
+        }
+    }
+    //HTTP POST (/like/comment/reply/$commentReplyId/$userId)
+    function likeCommentReply($commentReplyId, $userId){
+        try{
+            $this->likeService->addLikeCommentReply($commentReplyId, $userId);
+            return Response::apiResponse(Status::OK, 'like comment reply successfully', null);
+        }catch (Exception $e) {
+            return Response::apiResponse(Status::INTERNAL_SERVER_ERROR, $e->getMessage(), null);
+        }
+    }
+    //-------------------------------HTTP PUT --------------------------------
+    //-------------------------------HTTP DELETE --------------------------------
+    //HTTP DELETE (/like/post/$postId/$userId)
+    function unlikePost($postId, $userId){
+        try{
+            $this->likeService->deleteLikePost($postId, $userId);
+            return Response::apiResponse(Status::OK, 'unlike post successfully', null);
+        }catch (Exception $e) {
+            return Response::apiResponse(Status::INTERNAL_SERVER_ERROR, $e->getMessage(), null);
+        }
+    }
+    //HTTP DELETE (/like/comment/$comment/$userId)
+    function unlikeComment($postId, $userId){
+        try{
+            $this->likeService->deleteLikeComment($postId, $userId);
+            return Response::apiResponse(Status::OK, 'unlike comment successfully', null);
+        }catch (Exception $e) {
+            return Response::apiResponse(Status::INTERNAL_SERVER_ERROR, $e->getMessage(), null);
+        }
+    }
+    //HTTP DELETE (/like/comment/reply/$commentReplyId/$userId)
+    function deleteCommentReply($commentReplyId, $userId){
+        try{
+            $this->likeService->deleteLikeCommentReply($commentReplyId, $userId);
+            return Response::apiResponse(Status::OK, 'unlike commentReply successfully', null);
+        }catch (Exception $e) {
+            return Response::apiResponse(Status::INTERNAL_SERVER_ERROR, $e->getMessage(), null);
+        }
+    }
 }
