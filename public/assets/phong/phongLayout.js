0cbe00f506129f6582060bc6e532602db930aaf6
Phong <hongphongle774@gmail.com>
2023-11-01 21:39:22 +0700

    Load count notification unseen

--- public/assets/phong/phongLayout.js
+++ public/assets/phong/phongLayout.js
@@ -1,3 +1,6 @@
+$(document).ready(function () {
+    GetCountNotificationUnSeen();
+});
 $('#searchuser').keypress(function (event) {
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if (keycode == '13') {
@@ -7,3 +10,30 @@ $('#searchuser').keypress(function (event) {
         window.location.replace(url);
     }
 });
+function GetCountNotificationUnSeen() {
+    $.ajax({
+        url: 'http://localhost:8080/api/notification/countnotificationunseen',
+        method: 'GET',
+        contentType: 'json',
+        dataType: 'json',
+        async: false,
+        error: function (response) {
+        },
+        success: function (response) {
+            var count = response.data;
+            if (count !== 0) {
+                var html = `<span class="notification-count" style="position: absolute;
+    top: -5px;
+    right: -5px;
+    background-color: red;
+    color: white;
+    font-size: 12px;
+    padding: 2px 5px;
+    border-radius: 50%;">${count}</span>`;
+                $('#countnotificationunseen').html(html);
+            }
+        },
+        fail: function (response) {
+        }
+    });
+}
