1b3b29b3335939fa127bb8484c9a9e3cb191b83f
Astolfo-Saber <hungtd2180@gmail.com>
2023-11-04 09:41:14 +0700

    edit postInteract function

--- src/DAO/post_interact/PostInteractDAO.php
+++ src/DAO/post_interact/PostInteractDAO.php
@@ -61,7 +61,7 @@ class PostInteractDAO implements IPostInteractDAO
         $stmt = $this->connection->prepare("INSERT INTO post_interacts (user_id, post_id, content, type) VALUES (:user_id, :post_id, :content, :type)");
         $stmt->bindValue(':user_id', $userId);
         $stmt->bindValue(':post_id', $postId);
-        $stmt->bindValue(':content', '');
+        $stmt->bindValue(':content', 'Nhìn thấy không vui');
         $stmt->bindValue(':type','HIDDEN');
         $stmt->execute();
     }
